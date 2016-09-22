d092cb679e46b36e468f7ead6eb5dcb084400824
tuuleight <adarowski7@gmail.com>
2016-09-22 11:07:03 +0200

    class based views #1

--- interactive_calendar/urls.py
+++ interactive_calendar/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', views.MainPage.as_view(), name='index'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^my/$', views.events_my, name='my'),
-    url(r'^attended/$', views.events_attended, name='attended'),
-    url(r'^invited/$', views.events_invited, name='invited'),
+    url(r'^my/$', views.MyEvents.as_view(), name='my'),
+    url(r'^attended/$', views.AttendedEvents.as_view(), name='attended'),
+    url(r'^invited/$', views.InvitedEvents.as_view(), name='invited'),
     url(r'^event/(?P<pk>[0-9]+)/$', views.event_page, name='event_page'),
-]
+    ]
--- interactive_calendar/views.py
+++ interactive_calendar/views.py
@@ -1,36 +1,55 @@
+import datetime
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from .models import Event
 from .forms import EventForm, UserForm
-import datetime
 
 
-def register(request):
+class RegisterView(View):
+    template_name = 'interactive_calendar/register.html'
+    user_form = UserForm
     registered = False
 
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
+    def get(self, request, *args, **kwargs):
+        form = self.user_form()
+        registered = self.registered
+
+        return render(request, self.template_name, {'form': form,
+                                                    'registered': registered})
+
+    def post(self, request, *args, **kwargs):
+        form = self.user_form(request.POST)
+
+        if form.is_valid():
+            user = form.save()
             user.set_password(user.password)
             user.save()
             registered = True
-    else:
-        user_form = UserForm()
-        return render(request, 'interactive_calendar/register.html',
-                      {'user_form': user_form,
-                       'registered': registered}
-                      )
+        else:
+            return HttpResponse('User already created')
+
+        return render(request, self.template_name, {'form': form,
+                                                    'registered': registered})
 
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+class LoginView(View):
+    template_name = 'interactive_calendar/login.html'
+    user_form = UserForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.user_form()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        username = self.request.POST.get('username')
+        password = self.request.POST.get('password')
 
         user = authenticate(username=username, password=password)
 
@@ -43,77 +62,95 @@ def user_login(request):
         else:
             return HttpResponse('Sorry, invalid login')
 
-    else:
-        return render(request, 'interactive_calendar/login.html', {})
 
-
-@login_required
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
 
-def index(request):
-    if 'post_event' in request.POST:
-        form = EventForm(request.POST)
-        if form.is_valid():
-            event = form.save(commit=False)
-            event.author = request.user
-            event.attenders = None
-            event.invited = None
-            event.save()
-            return redirect('index')
-    else:
-        form = EventForm()
+class MainPage(View):
+    """
+    Main page with form to create new events and calendar to search for events
+    """
+    form_class = EventForm
+    template_name = 'interactive_calendar/index.html'
 
-    if 'datepicker' in request.POST:
-        event_date = request.POST.get('datepicker')
-        event_date = datetime.datetime.strptime(event_date, '%m/%d/%Y').date()
-        ev = Event.objects.filter(date_start__year=event_date.year,
-                                  date_start__month=event_date.month,
-                                  date_start__day=event_date.day,
-                                  private=False)
-        return render(request, 'interactive_calendar/index.html',
-                      {'form': form, 'ev': ev})
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
-    return render(request, 'interactive_calendar/index.html', {'form': form})
+    def post(self, request, *args, **kwargs):
+        """handle post request from calendar search and filled form"""
+        form = self.form_class()
 
+        if 'datepicker' in request.POST:
+            event_date = request.POST.get('datepicker')
+            event_date = datetime.datetime.strptime(event_date,
+                                                    '%m/%d/%Y').date()
+            ev = Event.objects.filter(date_start__year=event_date.year,
+                                      date_start__month=event_date.month,
+                                      date_start__day=event_date.day,
+                                      private=False)
+            return render(request, self.template_name, {'form': form, 'ev': ev})
 
-@login_required
-def events_my(request):
-    event = Event.objects.filter(author=request.user).order_by('date_start')
-    return render(
-        request, 'interactive_calendar/event_list.html',
-        {'event': event})
-
-
-@login_required
-def events_attended(request):
-    author = str(request.user)
-    event = Event.objects.filter(attenders__contains=[author])
-    return render(
-        request, 'interactive_calendar/event_list.html',
-        {'event': event})
+        else:
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                event = form.save(commit=False)
+                event.author = request.user
+                event.attenders = None
+                event.invited = None
+                event.save()
+                return redirect('index')
+            return render(request, self.template_name, {'form': form})
 
 
-@login_required
-def events_invited(request):
-    author = str(request.user)
-    event = Event.objects.filter(invited__contains=[author])
-    return render(
-        request, 'interactive_calendar/event_list.html',
-        {'event': event})
+class EventsList(ListView):
+    """
+    Class providing template and context to inheriting events list class
+    """
+    template_name = 'interactive_calendar/event_list.html'
+    context_object_name = 'event'
 
 
-@login_required
+class MyEvents(EventsList):
+    """
+    For events created by current user
+    """
+    def get_queryset(self):
+        return Event.objects.filter(author=self.request.user).order_by(
+            'date_start')
+
+
+class AttendedEvents(EventsList):
+    """
+    For events in which current user participates
+    """
+    def get_queryset(self):
+        author = str(self.request.user)
+        return Event.objects.filter(attenders__contains=[author])
+
+
+class InvitedEvents(EventsList):
+    """
+    For events
+    """
+    def get_queryset(self):
+        author = str(self.request.user)
+        return Event.objects.filter(invited__contains=[author])
+
+
 def event_page(request, pk):
     event = get_object_or_404(Event, pk=pk)
 
     if 'invite' in request.POST:
         username = request.POST.get('username')
-        if User.objects.filter(username=str(username)).exists():
+        us = User.objects.filter(username=str(username)).exists()
+        if us and event.invited is not None:
             event.invited.append(str(username))
             event.save()
+        else:
+            event.invited = [str(username)]
 
     if 'attend' in request.POST:
         if event.attenders is None:
@@ -146,3 +183,4 @@ def event_page(request, pk):
     return render(
         request, 'interactive_calendar/event_page.html',
         {'event': event, }, )
+
